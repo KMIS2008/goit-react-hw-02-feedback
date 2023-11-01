2020badce97c842ed48a20d84244b80d8251f322
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-11-01 22:56:24 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,6 @@ export const App = () => {
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
       React homework template
